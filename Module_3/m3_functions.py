6803e4420bbb7566d5443f504d6c6396faa25891
Igor Isachenkov <45761176+ivisav@users.noreply.github.com>
2021-12-23 15:07:00 +0300

    m3_functions.py: m3. functions example added

---
+++ Module_3/m3_functions.py
@@ -0,0 +1,51 @@
+def my_shiny_function():  # The name of your function
+    msg = "It's my new shiny function"  # "body" of your function
+    return msg  # The "output" of your function
+
+
+print('Example 1 result:')
+print(my_shiny_function())  # You need to refer to you function to get the result
+
+
+# You can translate data to functions
+
+def greeting_with_name(name):  # we want to send name to a function
+    msg = f"Hi {name}!!"  # we are using the variable
+    return msg
+
+
+print('Example 2 result:')
+print(greeting_with_name('Mike'))  # we need to provide value to function.
+
+# You can call your function in cycle to get new result per each object in list
+print('Example 3 result:')
+names = ['Vasia', 'Lena', 'Nick']
+for name in names:
+    print(greeting_with_name(name))
+
+
+def say_hi(*name):  # You can use arguments instead of loops
+    for name in names:
+        print(f"Hey {name}")
+
+
+print('Example 4 result:')
+say_hi("Bob", "Mike", "Nick")
+
+
+def func_args_exmp(name='Bob'):
+    msg = f"Hi {name} !"
+    return msg
+
+
+print('Example 5 result:')
+print(func_args_exmp())  # You can provide nothing to function - the result will be default value from function
+
+
+def say_smth(name, msg):
+    output = f"{msg} {name}"
+    return output
+
+
+print('Example 6 result:')
+print(say_smth(name='Bob', msg='Hey'))  # you can assign value explicitly
