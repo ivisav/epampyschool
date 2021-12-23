ccb6f166575034d2c113b7714fea72609eddc26b
commondude <37772073+commondude@users.noreply.github.com>
2021-12-23 13:53:33 +0300

    Add example formatation with '.format' strings. (#1)
    * Add example formatation with '.format' strings
    
    * Add example formatation with '.format' strings. Fix

--- Module_2/m2_strings.py
+++ Module_2/m2_strings.py
@@ -6,7 +6,10 @@ print('Only 1st symbol: %(exp_part)s' % {"exp_part": expression[0]})  # string w
 
 print(f"Let's cut only name: {expression[5:]}")  # F-string
 
-print('First part w/o name: {}'.format(expression[0:4]))  # string with .format 
+print('First part w/o name: {}'.format(expression[0:4]))  # string with .format
+
+print('First is {2}, second is {0}, third is {1}. '.format('first', 'second', 'third')) # We can choose
+# particular argument. Index like in list
 
 print("Character one by one:")
 for character in expression:
