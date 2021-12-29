9298ccbaf741b4a7fc4afc870929ac38ecd5f910
Igor Isachenkov <45761176+ivisav@users.noreply.github.com>
2021-12-29 22:17:12 +0300

    m4_class_example.py more visual appealing example

--- Module_4/m4_class_example.py
+++ Module_4/m4_class_example.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Car:
     def __init__(self, year, model, vendor, color='black'):
         self.year = year
@@ -13,21 +15,29 @@ class Car:
             return 'Only calm ride for you'
 
     def getinfosold(self):
-        return (f"your car sold by '{self.vendor}' in {self.year}")
+        return (f"Your {self.color} {self.model} sold by '{self.vendor}' in {self.year}")
 
     @classmethod
-    def get_sold_info(cls, year, vendor):
-        return cls(year, vendor)
+    def get_sold_ago(cls, age, model, vendor, color):
+        calc_year = date.today().year - age
+        return cls(calc_year, model, vendor='NO_NAME', color='White')
 
 
 # Object descriptions
 car_1 = Car(2021, 'Focus', 'Ford Motorzz', 'gray')
-car_2 = Car(2021, '3', 'Mazda Cars', 'red')
+car_2 = Car(2011, 'Mazda 3', 'Mazda Cars', 'red')
+
+print(car_1.getinfosold())
+print(car_2.getinfosold())
 
 # Output for staticmethod
 print(Car.car_check('sport'))
 
 # Output for classmethod
+# we don't know the year when the car was produced, lets calculate based on age
 
-print(car_1.getinfosold())
-print(car_2.getinfosold())
+car_3 = Car.get_sold_ago(10, 'Mazda 6', 'Mazda Cars', 'red')
+
+# Also, vendor and color will be changed
+print(f"It was sold in {car_3.year} ")
+print(car_3.getinfosold())
